c8ad7f2da25533d28d4233cfc615127f988256ce
Angus Gratton <gus@projectgus.com>
2011-09-05 22:51:05 +1000

    Cleanup Win32 code, make work on port name not USB parameters

--- src/fixlatency_win32.cpp
+++ src/fixlatency_win32.cpp
@@ -7,81 +7,69 @@
 #include "qextserialport/qextserialenumerator.h"
 #include <winreg.h>
 
-static bool findUSBIds(QString physName, int *vendorId, int *productId)
+// Return the windows registry entry for the FTDI device (open), or 0
+HKEY openFTDIDeviceParameters(QString portName, QString &errorMessage)
 {
-    QList<QextPortInfo> ports = QextSerialEnumerator::getPorts();
-    for(QList<QextPortInfo>::iterator it = ports.begin(); it != ports.end(); it++) {
-        if(it->physName == physName) {
-            *vendorId = it->vendorID;
-            *productId = it->productID;
-            return true;
-        }
-    }
-    return false;
-}
-
-bool fixLatency(QString portPhysName, QString &errorMessage)
-{
-    int vid = 0,pid = 0;
-
-    if(!findUSBIds(portPhysName, &vid, &pid) || vid == 0 || pid == 0) {
-        errorMessage.sprintf("Not a USB device");
-        return false; // Not USB, so definitely not FTDI
-    }
-
     HKEY key;
     const wchar_t *keypath = L"SYSTEM\\CurrentControlSet\\Enum\\FTDIBUS";
     long r = RegOpenKeyEx(HKEY_LOCAL_MACHINE, keypath,
                             0, KEY_READ, &key);
     if(r) {
-        errorMessage.sprintf("Failed to open parent FTDIBUS key. No FTDI driver?");
-        return false;
+        errorMessage.sprintf("Failed to open parent FTDIBUS key. No FTDI driver installed?");
+        return 0;
     }
 
-    // subkeys of FTDIBUS look like VID_0403+PID_6001+A6006eL7A
-    QString prefix = QString("VID_%1+PID_%2").arg(vid,4,16,QChar('0')).arg(pid,4,16,QChar('0'));
-
-    // Look through available subkeys
+    // Look through available FTDI devices
     int index = 0;
-    const int SUBKEY_SZ = 140;
-    wchar_t subkey[SUBKEY_SZ];
-    while(r == 0) {
-        DWORD keysz = SUBKEY_SZ;
-        r = RegEnumKeyEx(key, index++, subkey, &keysz, 0, NULL, NULL, NULL);
-        if (r == 0 && QString::fromWCharArray(subkey).startsWith(prefix))
-            break;
-    }
-
-    if(r == ERROR_NO_MORE_ITEMS) {
-        errorMessage.sprintf("Not found, prefix %s tried %d alternatives", prefix.toAscii().data(), index);
-        RegCloseKey(key); // Not found
-        return false;
-    }
-    else if(r) {
-        errorMessage.sprintf("Failed to enumerate FTDIBUS keys");
-        RegCloseKey(key);
-        return false;
-    }
-
     HKEY paramKey = 0;
-    QString paramPath = QString("%1\\0000\\Device Parameters").arg(QString::fromWCharArray(subkey));
-    if(paramPath.length() >= SUBKEY_SZ) {
-        errorMessage.sprintf("Internal error: Subkey path %s is too long for buffer", paramPath.toAscii().data());
-        RegCloseKey(key);
-        return false;
+    while(1) {
+        if(paramKey) {
+          RegCloseKey(paramKey);
+          paramKey = 0;
+        }
+        DWORD subkey_sz = 140;
+        wchar_t subkey[subkey_sz];
+        r = RegEnumKeyEx(key, index++, subkey, &subkey_sz, 0, NULL, NULL, NULL);
+        if(r) {
+            if(r==ERROR_NO_MORE_ITEMS)
+                errorMessage.append("FTDI device not found");
+            else
+                errorMessage.sprintf("Failed to enumerate FTDIBUS keys");
+            break;
+        }
+        QString paramPath = QString("%1\\0000\\Device Parameters").arg(QString::fromWCharArray(subkey));
+        errorMessage.append(paramPath).append("  ");
+        r = RegOpenKeyEx(key, paramPath.toStdWString().c_str(), 0, KEY_READ|KEY_SET_VALUE, &paramKey);
+        if(r) {
+            continue;
+        }
+        DWORD regPortName_sz = 10;
+        wchar_t regPortName[regPortName_sz];
+        memset(regPortName, 0, sizeof(regPortName));
+        DWORD type;
+        errorMessage.append(" about to load PortName ");
+        r = RegQueryValueEx(paramKey, L"PortName", NULL, &type, (LPBYTE)&regPortName, &regPortName_sz);
+        if(!r && portName == QString::fromWCharArray(regPortName)) {
+            break;
+        }
     }
-    paramPath.toWCharArray(subkey);
-    r = RegOpenKeyEx(key, subkey, 0, KEY_READ|KEY_SET_VALUE, &paramKey);
-    if(r) {
-        errorMessage.sprintf("Failed to open subkey %s (error %lx)", QString::fromWCharArray(subkey).toAscii().data(), r);
-        RegCloseKey(key);
+    RegCloseKey(key);
+    return paramKey;
+}
+
+bool fixLatency(QString portName, QString &errorMessage)
+{
+    HKEY paramKey = openFTDIDeviceParameters(portName, errorMessage);
+
+    if(!paramKey)
         return false;
-    }
+
+    // if we get here, paramKey is the subkey with Device Parameters for our port
 
     DWORD latency, type, lsize = sizeof(DWORD);
-    r = RegQueryValueEx(paramKey, L"LatencyTimer", NULL, &type, (LPBYTE)&latency, &lsize);
+    int r = RegQueryValueEx(paramKey, L"LatencyTimer", NULL, &type, (LPBYTE)&latency, &lsize);
     if(r) {
-        errorMessage.sprintf("Failed to open LatencyTimer value %s", paramPath.toAscii().data());
+        errorMessage.sprintf("Failed to read LatencyTimer value");
     } else if(type != REG_DWORD) {
         errorMessage.sprintf("Got unexpected type for LatencyTimer");
     } else if(latency > 1) {
@@ -95,6 +83,5 @@ bool fixLatency(QString portPhysName, QString &errorMessage)
     }
 
     RegCloseKey(paramKey);
-    RegCloseKey(key);
     return r == 0;
 }
